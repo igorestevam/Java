d0514ee51da3f2ddde40729af0b26d051bdc5124
Igor Estevam <igorestevam1900@gmail.com>
2025-09-25 10:57:24 -0300

    Create main.java

---
+++ RPG/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rpg;
+
+/**
+ *
+ * @author alunolab10
+ */
+public class main {
+
+    public static void main(String[] args) {
+        
+    }
+}
