cce8b86c2c40a5b46b837541134a6917f9dee8c5
Igor Estevam <igorestevam1900@gmail.com>
2025-09-26 10:46:46 -0300

    Update main.java

--- RPG/main.java
+++ RPG/main.java
@@ -6,11 +6,18 @@ package com.mycompany.rpg;
 
 /**
  *
- * @author alunolab10
+ * @author alunodev10
  */
+import java.util.ArrayList;
 public class main {
-
     public static void main(String[] args) {
+        ArrayList<Inimigo> inimigos;
+        Inimigo goblin = new Inimigo("facil", "Goblin", 15, 2, 7, 2, 1, 10);
+        Inimigo lobo = new Inimigo("facil", "Lobo", 13, 3, 7, 2, 1, 10);
+        Inimigo slime = new Inimigo("facil", "Slime", 10, 4, 7, 2, 1, 10);
+        Inimigo ogro = new Inimigo("medio", "Ogro", 25, 6, 10, 4, 2, 20);
+        Inimigo esqueleto = new Inimigo("medio", "Esqueleto", 22, 8, 10, 4, 2, 20);
+        Inimigo fantasma = new Inimigo("medio", "Fantasma", 19, 7, 10, 4, 2, 20);
         
     }
 }
